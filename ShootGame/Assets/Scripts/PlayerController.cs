9817c14ad3b086e8e88065830287ce66c94fb33d
aschia <ajs1173@utulsa.edu>
2021-09-13 16:41:21 -0500

    Created player object n started controller script
    See title

---
+++ ShootGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+/*
+ * Created by aschia
+ * Date Created: Sept 13
+ * 
+ * Last Edited: Sept 13
+ * 
+ * Desc: Player movements n stuff
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
